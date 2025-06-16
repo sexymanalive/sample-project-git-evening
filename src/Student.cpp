c034cc0e4470b76ea4a02f8439a3e2557233ee29
sexymanalive <lyvanna544@gmail.com>
2025-06-16 12:46:58 +0000

    feature: add student

---
+++ src/Student.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std; 
+
+class Student{
+    public: 
+        string name; 
+        int age; 
+}; 
+
+void main(){
+    cout<<"STudent demo "<<endl; 
+    return 0 ; 
+}
