368b78dbaaf07a4353282fe28c8a77e8b1cec59a
sexymanalive <lyvanna544@gmail.com>
2025-06-16 13:16:18 +0000

    adding teacher class too

--- src/Teacher.cpp
+++ src/Teacher.cpp
@@ -1,6 +1,15 @@
 #include<iostream> 
 using namespace std; 
 
+class Teacher{
+    private: 
+        string name; 
+        int age; 
+}; 
+
+void helloworld(){
+    cout<<"this hello world function "<<endl; 
+}
 int main(){
     system("clear"); 
 
